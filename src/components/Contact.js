67a705e0d26eddacfaf507968b0cd358fb2036dd
unknown <ohj12@danal.co.kr>
2017-07-11 12:22:28 +0900

    implements searching

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
 
     constructor(props) {
+        //react-hot-loader 기능은 생성자를 호출하지 않음에 주의
         super(props);
         this.state = {
+            selectedKey: -1,
+            keyword: '',
             contactData: [{
                 name: 'Abet',
                 phone: '010-0000-0001'
@@ -20,19 +25,52 @@ export default class Contact extends React.Component {
                 phone: '010-0000-0004'
             }]
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+      this.setState({
+        keyword: e.target.value
+      });
+    }
+
+    handleClick(key){
+      this.setState({
+        selectedKey : key
+      });
+
+      console.log(key, 'is selected');
     }
 
     render() {
         const mapToComponents = (data) => {
+            data.sort();
+            data = data.filter( (contact) => {
+              return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+            } );
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (
+                  <ContactInfo
+                    contact={contact}
+                    key={i}
+                    onClick={()=>{ this.handleClick(i)}}/>
+                );
             });
         };
 
         return (
             <div>
                 <h1>Contacts</h1>
+                <input name="keyword" placeholder="Search"
+                  onChange={this.handleChange}
+                  value={this.state.keyword}/>
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails
+                  isSelected={this.state.selectedKey != -1}
+                  contact={this.state.contactData[this.state.selectedKey]}
+                />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
 
-ReactDOM.render(
-  <AppContainer>
-    <App/>
-  </AppContainer>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
 
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const NextApp = require('./components/App').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp/>
-      </AppContainer>
-      ,
-      document.getElementById('root')
-    );
-  });
-}
+
+
+//cmd run
+//npm run dev-server
+//create react app 자동 프로젝트 생성 툴
